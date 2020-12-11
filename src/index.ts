4c1e41f7db2cfc5f2a5fb2890046c945b75ebad1
daxindg <my@example.com>
2020-12-11 10:33:31 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-import { ApolloServer } from 'apollo-server-express';
-import connectRedis from 'connect-redis';
-import dotenv from 'dotenv-safe';
-import express from 'express';
-import session, { SessionOptions } from 'express-session';
-import Redis from 'ioredis';
-import path from 'path';
-import 'reflect-metadata';
-import { buildSchema } from 'type-graphql';
-import { createConnection } from 'typeorm';
+import { ApolloServer } from "apollo-server-express";
+import connectRedis from "connect-redis";
+import dotenv from "dotenv-safe";
+import express from "express";
+import session, { SessionOptions } from "express-session";
+import Redis from "ioredis";
+import path from "path";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
 import { __prod__ } from "./constants";
-import { Article } from './entities/Article';
-import { Borrow } from './entities/Borrow';
-import { Journal } from './entities/Journal';
-import { Issue } from './entities/Issue';
-import { User } from './entities/User';
+import { Article } from "./entities/Article";
+import { Borrow } from "./entities/Borrow";
+import { Journal } from "./entities/Journal";
+import { Issue } from "./entities/Issue";
+import { User } from "./entities/User";
 import { JournalResolver } from "./resolvers/journal";
-import { UserResolver } from './resolvers/user';
-import { MyContext } from './types/MyContext';
-import { IssueResolver } from './resolvers/issue';
+import { UserResolver } from "./resolvers/user";
+import { MyContext } from "./types/MyContext";
+import { IssueResolver } from "./resolvers/issue";
 
-
-const main = async ()=> {
+const main = async () => {
   dotenv.config();
   const conn = await createConnection({
     type: "postgres",
@@ -28,29 +27,29 @@ const main = async ()=> {
     migrations: [path.join(__dirname, "./migrations/*")],
     entities: [User, Journal, Article, Issue, Borrow],
     synchronize: true,
-    logging: !__prod__
+    logging: !__prod__,
   });
 
   await conn.runMigrations();
 
   const app = express();
   app.set("trust proxy", true);
-  
+
   const RedisStore = connectRedis(session);
   const redis = new Redis(process.env.REDIS_URL);
 
-  const sessionOption:SessionOptions = {
-    name: 'sid',
-    store: new RedisStore({ 
+  const sessionOption: SessionOptions = {
+    name: "sid",
+    store: new RedisStore({
       client: redis,
       disableTouch: true,
-      disableTTL: true
+      disableTTL: true,
     }),
     cookie: {
-      path:"/",
-      maxAge: 1000 * 3600* 24,
+      path: "/",
+      maxAge: 1000 * 3600 * 24,
       httpOnly: true,
-      sameSite: __prod__ ? "none":"lax",
+      sameSite: __prod__ ? "none" : "lax",
       secure: __prod__,
     },
     saveUninitialized: false,
@@ -60,26 +59,29 @@ const main = async ()=> {
 
   // console.log(sessionOption);
 
-  app.use(
-    session(sessionOption)
-  );
+  app.use(session(sessionOption));
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers:[JournalResolver, UserResolver, IssueResolver],
-      validate: false
+      resolvers: [JournalResolver, UserResolver, IssueResolver],
+      validate: false,
     }),
-    context: ({req, res}):MyContext => ({req, res, redis }),
+    introspection: true,
+    playground: true,
+    context: ({ req, res }): MyContext => ({ req, res, redis }),
   });
 
   const allowedOrigins = [process.env.CORS_URL, "http://localhost:3000"];
   // console.log(allowedOrigins);
 
-  apolloServer.applyMiddleware({app, cors: {origin: allowedOrigins, credentials: true}});
+  apolloServer.applyMiddleware({
+    app,
+    cors: { origin: allowedOrigins, credentials: true },
+  });
 
   app.listen(parseInt(process.env.PORT), () => {
     console.log(`server started on localhost:${process.env.PORT}`);
-  })
-}
+  });
+};
 
 main();
